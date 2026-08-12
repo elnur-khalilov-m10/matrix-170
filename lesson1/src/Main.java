11b0a18d8b2d92df1d9b82cdbcc2d01f0d70a44b
Feride Ismayilova <ferideismayilova340@gmail.com>
2026-08-12 19:29:05 +0400

    Add even numbers loop and sum calculation in Main class

--- lesson1/src/Main.java
+++ lesson1/src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
@@ -7,5 +5,16 @@ public class Main {
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
         }
+
+
+        for (int i = 0; i <= 10; i += 2) {
+            System.out.println("Even number: " + i);
+        }
+
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+        }
+        System.out.println("Sum of numbers 1-10: " + sum);
     }
 }
