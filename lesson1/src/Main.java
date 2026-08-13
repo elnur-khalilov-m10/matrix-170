70ddde94ae0c29abb536970f521bd9a5d59162fc
Nazila Manafova <nazmanafova9@gmail.com>
2026-08-12 21:45:55 -0700

    convert for loops to while

--- lesson1/src/Main.java
+++ lesson1/src/Main.java
@@ -2,18 +2,21 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        for (int i = 0; i < 10; i++) {
+        int i = 0;
+        while (i < 10) {
             System.out.println(i);
+            i++;
         }
 
-
-        for (int i = 0; i <= 10; i += 2) {
-            System.out.println("Even number: " + i);
+        i = 0;
+        while (i <= 10) {
+            System.out.println("Even numbers: " + i);
+            i += 2;
         }
 
         int sum = 0;
-        for (int i = 1; i <= 10; i++) {
-            sum += i;
+        for (int j = 1; j <= 10; j++) {
+            sum += j;
         }
         System.out.println("Sum of numbers 1-10: " + sum);
     }
